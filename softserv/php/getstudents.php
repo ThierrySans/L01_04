5a47083d29ad300d07537c1569502316304e3af4
Charles Ruan <yesilikechee@live.com>
2017-10-25 17:56:28 -0400

    Changed to have database be sqlite3 instead of mysql so we have local database storage.

--- softserv/php/getstudents.php
+++ softserv/php/getstudents.php
@@ -1,27 +1,33 @@
 
 <?php
 header('Content-Type: application/json');
-// php config
-$servername= 'localhost';
-$username = 'softserv_admin';
-$password = 'softserv';
-$db = 'softserv';
 
-// create a connection
-$conn = mysqli_connect($servername, $username, $password, $db);
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
+class database extends SQLite3 {
+	function __construct() {
+		$this->open('homework.db')
+	}
 }
 
-// Set SQL query and input the partial course name
-$sql_getstudents    = "SELECT * FROM STUDENTS";
+$conn = new database();
 
-$result_getstudents = mysqli_query($conn, $sql_getstudents);
+  if(!$conn) {
+      echo $conn->lastErrorMsg();
+   } else {
+      echo "Opened database successfully\n";
+   }
+
+// Set SQL query and input the partial course name
+$sql_getstudents =<<<EOF
+      SELECT * from STUDENTS;
+EOF;
+
+
+$result_getstudents = $conn->query($sql_getstudents);
 
 $frommysql_getstudents = array(); //retrieve from assoc array
 
 // GET students
-while ($row_getstudents = mysqli_fetch_assoc($result_getstudents)) {
+while ($row_getstudents = $result_getstudents->fetcharray(SQLITE3_ASSOC)) {
 	$return_getstudents[] = $row_getstudents;
 }
 $getstudents = array();
@@ -36,6 +42,6 @@ for ($i = 0; $i < count($return_getstudents); $i++) {
 }
 
 echo json_encode($getstudents);
-mysqli_close($conn);
+$conn->close();
 
 ?>
