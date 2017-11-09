58e05c916c9ca9baccd55a1c84dc29993a3881c4
Charles Ruan <yesilikechee@live.com>
2017-11-09 18:30:33 -0500

    Added php function to store grades into a database

---
+++ softserv/php/storegrades.php
@@ -0,0 +1,54 @@
+<?php
+header('Content-Type: application/json');
+// php config
+$servername= 'localhost';
+$username = 'softserv_admin';
+$password = 'softserv';
+$db = 'softserv';
+
+// create a connection
+$studentid = $_GET["username"];
+$problemsetid = $_GET["problemsetid"];
+$mark= $_GET["mark"];
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+$test_table = "SELECT * FROM PROBLEMSETGRADES";
+$get_highmark = "SELECT HIGHESTSCORE FROM PROBLEMSETGRADES WHERE PROBLEMSETID = $problemsetid AND StudentID = $studentid";
+$result = mysqli_query($conn, $test_table);
+
+//If no table
+if (empty($result)) {
+	$query = "CREATE TABLE PROBLEMSETGRADES (
+		ProblemsetID Int not null,
+		StudentID Int not null,
+		HighestScore Int not null default 0,
+		RecentScore Int not null default 0,
+		primary key (ProblemsetID, StudentID)
+		)";
+		mysqli_query($conn, $query);
+		$sql_insert_marks= "INSERT INTO PROBLEMSETGRADES(ProblemsetID, StudentID, HighestScore, RecentScore) VALUES('$studentid','$problemsetid', '$mark', '$mark')";
+		
+} else {
+	$highmark = mysqli_query($conn, $sql_highmark);
+	//If student or problemset not in table
+	if (empty($highmark)) {
+		$query = "INSERT INTO PROBLEMSETGRADES(ProblemsetID, StudentID, HighestScore, RecentScore) VALUES('$studentid','$problemsetid', '$mark', '$mark')";
+		$result = mysqli_query($conn, $query);
+	} else {
+		$highmark = mysqli_fetch_assoc($highmark);
+		$highmark = highmark["HighestScore"];
+		if ($highmark > $mark) {
+			$query = "UPDATE PROBLEMSETGRADES SET RecentScore = $mark WHERE ProblemsetID = $problemsetid AND StudentID = $studentid";
+			$result = mysqli_query($conn, $query);
+		} else {
+			$query = "UPDATE PROBLEMSETGRADES SET RecentScore = $mark, HighestScore = $mark WHERE ProblemsetID = $problemsetid AND StudentID = $studentid";
+			$result = mysqli_query($conn, $query);
+		}
+	}	
+}
+echo json_encode($result);
+mysqli_close($conn);
+?>
