06aafc3b67385b5c539c4b684c8c7c0a75b428d0
Mahesh209 <31778169+Mahesh209@users.noreply.github.com>
2017-11-19 15:29:34 -0500

    Update loginTest.php

--- Test/phpUnitTests/loginTest.php
+++ Test/phpUnitTests/loginTest.php
@@ -10,11 +10,11 @@ class loginTest extends TestCase
 	
 	public function password_validation_when_match_test()
 	{
-		$this->assertEquals(password_validation("password", "password")};
+		$this->assertEquals(password_validation("password", "password"));
 	}
 	public function password_validation_when_not_matching_but_same_length_test()
 	{
-		$this->assertNotEquals(password_validation("password0", "password1")};
+		$this->assertNotEquals(password_validation("password0", "password1"));
 	}
 	public function password_validation_when_not_matching_test()
 	{
