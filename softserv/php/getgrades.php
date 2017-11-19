1f2b6801035fc283c2185c43ffc31dc56c5a3f2f
Charles Ruan <yesilikechee@live.com>
2017-11-19 12:42:42 -0500

    Refactored grades files to functions

--- softserv/php/getgrades.php
+++ softserv/php/getgrades.php
@@ -2,54 +2,57 @@
 header('Content-Type: application/json');
 include('./config.php');
 
-// create a connection
-$conn = mysqli_connect($servername, $username, $password, $db);
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-
-$studentid = $_GET["username"];
-
-$sql_getgrades= "SELECT PROBLEMSETS.ID AS ID, UNITID, UNITS.NAME AS UNITNAME, PROBLEMSETS.NAME AS NAME, DATEDUE, HighestScore, RecentScore FROM (PROBLEMSETGRADES RIGHT JOIN PROBLEMSETS ON ID = ProblemsetID AND StudentID = '$studentid') JOIN UNITS ON UNITS.ID = PROBLEMSETS.UNITID;";
-
-$result_getgrades= mysqli_query($conn, $sql_getgrades);
-
-$frommysql_getgrades = array(); //retrieve from assoc array
-
-// GET grades
-while ($row_getgrades = mysqli_fetch_assoc($result_getgrades)) {
-	$return_getgrades[] = $row_getgrades;
-}
-
-$grades= array();
-
-for ($i = 0; $i < count($return_getgrades); $i++) {
-	$problemsetid = $return_getgrades[$i]["ID"];
-	$unitid = $return_getgrades[$i]["UNITID"];
-	$unitname = $return_getgrades[$i]["UNITNAME"];
-	$problemsetname = $return_getgrades[$i]["NAME"];
-	$datedue = $return_getgrades[$i]["DATEDUE"];
-	
-	
-	$highestscore = $return_getgrades[$i]["HighestScore"];
-	if ($highestscore == null) {
-		$highestscore = 0;
+function getGrades() {
+	// create a connection
+	$conn = mysqli_connect($servername, $username, $password, $db);
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
 	}
-	
-	$recentscore = $return_getgrades[$i]["RecentScore"];
-	if ($recentscore == null) {
-		$recentscore = 0;
-	}
-	
 
-	$grades[$unitid]["unitdata"][$problemsetid] = array("name" => $problemsetname,
-		  "datedue" => $datedue,
-		  "highestscore" => $highestscore,
-		  "recentscore" => $recentscore);
-	$grades[$unitid]["unitname"] = $unitname;
+	$studentid = $_GET["username"];
+
+	$sql_getgrades= "SELECT PROBLEMSETS.ID AS ID, UNITID, UNITS.NAME AS UNITNAME, PROBLEMSETS.NAME AS NAME, DATEDUE, HighestScore, RecentScore FROM (PROBLEMSETGRADES RIGHT JOIN PROBLEMSETS ON ID = ProblemsetID AND StudentID = '$studentid') JOIN UNITS ON UNITS.ID = PROBLEMSETS.UNITID;";
+
+	$result_getgrades= mysqli_query($conn, $sql_getgrades);
+
+	$frommysql_getgrades = array(); //retrieve from assoc array
+
+	// GET grades
+	while ($row_getgrades = mysqli_fetch_assoc($result_getgrades)) {
+		$return_getgrades[] = $row_getgrades;
+	}
+
+	$grades= array();
+
+	for ($i = 0; $i < count($return_getgrades); $i++) {
+		$problemsetid = $return_getgrades[$i]["ID"];
+		$unitid = $return_getgrades[$i]["UNITID"];
+		$unitname = $return_getgrades[$i]["UNITNAME"];
+		$problemsetname = $return_getgrades[$i]["NAME"];
+		$datedue = $return_getgrades[$i]["DATEDUE"];
+		
+		
+		$highestscore = $return_getgrades[$i]["HighestScore"];
+		if ($highestscore == null) {
+			$highestscore = 0;
+		}
+		
+		$recentscore = $return_getgrades[$i]["RecentScore"];
+		if ($recentscore == null) {
+			$recentscore = 0;
+		}
+		
+
+		$grades[$unitid]["unitdata"][$problemsetid] = array("name" => $problemsetname,
+			  "datedue" => $datedue,
+			  "highestscore" => $highestscore,
+			  "recentscore" => $recentscore);
+		$grades[$unitid]["unitname"] = $unitname;
+	}
+	mysqli_close($conn);
+	return $grades;
+	
 }
 
-echo json_encode($grades);
-mysqli_close($conn);
-
+json_encode(getGrades());
 ?>
--- softserv/php/storegrades.php
+++ softserv/php/storegrades.php
@@ -1,52 +1,57 @@
 <?php
 header('Content-Type: application/json');
 include('./config.php');
-// create a connection
-$studentid = $_GET["username"];
-$problemsetid = $_GET["problemsetid"];
-$mark= $_GET["mark"];
+function storeGrades($studentid, $problemsetid, $mark) {
+	// create a connection
 
-$conn = mysqli_connect($servername, $username, $password, $db);
-if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
-}
-$test_table = "SELECT * FROM PROBLEMSETGRADES";
-$get_highmark = "SELECT HighestScore FROM PROBLEMSETGRADES WHERE ProblemsetID = $problemsetid AND StudentID = '$studentid'";
-$result = mysqli_query($conn, $test_table);
+	$conn = mysqli_connect($servername, $username, $password, $db);
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	$test_table = "SELECT * FROM PROBLEMSETGRADES";
+	$get_highmark = "SELECT HighestScore FROM PROBLEMSETGRADES WHERE ProblemsetID = $problemsetid AND StudentID = '$studentid'";
+	$result = mysqli_query($conn, $test_table);
 
-//If no table
-if ($result == false) {
-	$query = "CREATE TABLE PROBLEMSETGRADES (
-		ProblemsetID Int not null,
-		StudentID VARCHAR(20) not null,
-		HighestScore Int not null default 0,
-		RecentScore Int not null default 0,
-		primary key (ProblemsetID, StudentID)
-		)";
-		mysqli_query($conn, $query);
-		$sql_insert_marks= "INSERT INTO PROBLEMSETGRADES(ProblemsetID, StudentID, HighestScore, RecentScore) VALUES('$studentid', $problemsetid, $mark, $mark)";
-	    mysqli_query($conn, $sql_insertmarks);
-	    $highmark = array("hello" => "hi");
-		//new entry in new table
-		
-} else {
-	$highmark_row = mysqli_query($conn, $get_highmark);
-	//If student or problemset not in table
-	if (mysqli_num_rows($highmark_row) == 0) {
-		$query = "INSERT INTO PROBLEMSETGRADES(ProblemsetID, StudentID, HighestScore, RecentScore) VALUES($problemsetid,'$studentid', $mark, $mark)";
-		$result = mysqli_query($conn, $query);
+	//If no table
+	if ($result == false) {
+		$query = "CREATE TABLE PROBLEMSETGRADES (
+			ProblemsetID Int not null,
+			StudentID VARCHAR(20) not null,
+			HighestScore Int not null default 0,
+			RecentScore Int not null default 0,
+			primary key (ProblemsetID, StudentID)
+			)";
+			mysqli_query($conn, $query);
+			$sql_insert_marks= "INSERT INTO PROBLEMSETGRADES(ProblemsetID, StudentID, HighestScore, RecentScore) VALUES('$studentid', $problemsetid, $mark, $mark)";
+			mysqli_query($conn, $sql_insertmarks);
+			$highmark = array("hello" => "hi");
+			//new entry in new table
+			
 	} else {
-		$highmark_row = mysqli_fetch_assoc($highmark_row);
-		$highmark = $highmark_row["HighestScore"];
-		if ($highmark > $mark) {
-			$query = "UPDATE PROBLEMSETGRADES SET RecentScore = $mark WHERE ProblemsetID = $problemsetid AND StudentID = '$studentid'";
+		$highmark_row = mysqli_query($conn, $get_highmark);
+		//If student or problemset not in table
+		if (mysqli_num_rows($highmark_row) == 0) {
+			$query = "INSERT INTO PROBLEMSETGRADES(ProblemsetID, StudentID, HighestScore, RecentScore) VALUES($problemsetid,'$studentid', $mark, $mark)";
 			$result = mysqli_query($conn, $query);
 		} else {
-			$query = "UPDATE PROBLEMSETGRADES SET RecentScore = $mark, HighestScore = $mark WHERE ProblemsetID = $problemsetid AND StudentID = '$studentid'";
-			$result = mysqli_query($conn, $query);
-		}
-	}	
+			$highmark_row = mysqli_fetch_assoc($highmark_row);
+			$highmark = $highmark_row["HighestScore"];
+			if ($highmark > $mark) {
+				$query = "UPDATE PROBLEMSETGRADES SET RecentScore = $mark WHERE ProblemsetID = $problemsetid AND StudentID = '$studentid'";
+				$result = mysqli_query($conn, $query);
+			} else {
+				$query = "UPDATE PROBLEMSETGRADES SET RecentScore = $mark, HighestScore = $mark WHERE ProblemsetID = $problemsetid AND StudentID = '$studentid'";
+				$result = mysqli_query($conn, $query);
+			}
+		}	
+	}
+	mysqli_close($conn);
+	return (json_encode($query));
 }
-echo json_encode($query);
-mysqli_close($conn);
+
+$student = $_GET["username"];
+$problemset = $_GET["problemsetid"];
+$grade = $_GET["mark"];
+
+json_encode(storeGrades($student, $problemset, $grade));
 ?>
