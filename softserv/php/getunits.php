cd071c5ea43439161a18dacafbc82243972c8acd
unknown <mahesh.singh@mail.utoronto.ca>
2017-11-18 22:41:32 -0500

    put functionality of files into functions to allow us to unit test them

--- softserv/php/getunits.php
+++ softserv/php/getunits.php
@@ -1,28 +1,40 @@
 <?php
 header('Content-Type: application/json');
 include('./config.php');
-// create a connection
+
+// create a connection and return it
+
 $conn = mysqli_connect($servername, $username, $password, $db);
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
+
+// query the database of a given connection and return the units
 
 // Set SQL query and input the partial course name
 $sql_getunits = "SELECT * FROM UNITS";
 
 $result_getunits = mysqli_query($conn, $sql_getunits);
 
-// GET students
+// GET units
 while ($row_getunits = mysqli_fetch_assoc($result_getunits)) {
 	$return_getunits[] = $row_getunits;
 }
-$getunits = array();
-for ($i = 0; $i < count($return_getunits); $i++) {
-	$unitid = $return_getunits[$i]["ID"];
-	$name = $return_getunits[$i]["NAME"];
 
-	$getunits[$unitid] = $name;
+// split the units into an array
+function get_units($query_result){ 
+	$getunits = array();
+	for ($i = 0; $i < count($query_result); $i++) {
+		$unitid = $query_result[$i]["ID"];
+		$name = $query_result[$i]["NAME"];
+
+		$getunits[$unitid] = $name;
+	}
+	return $getunits;
 }
+
+// call function to execute the task of getting units
+$getunits = get_units($return_getunits);
 
 echo json_encode($getunits);
 mysqli_close($conn);
--- softserv/php/login.php
+++ softserv/php/login.php
@@ -5,21 +5,26 @@ $studentid = $_GET["username"];
 $studentpassword = $_GET["password"];
 
 // create a connection
+
 $conn = mysqli_connect($servername, $username, $password, $db);
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
 
 // Set SQL query and input the partial course name
-
 $sql_compare= "SELECT PASSWORD FROM STUDENTS WHERE UTORID = '$studentid'";
 
 $expected_hashed_password = mysqli_query($conn, $sql_compare);
 $expected_hashed_password = mysqli_fetch_assoc($expected_hashed_password);
 $expected_hashed_password = $expected_hashed_password["PASSWORD"];
-$cmp_result = strcmp("$expected_hashed_password","$studentpassword");
-echo json_encode($cmp_result);
-// echo json_encode($expected_hashed_password, password_hash($studentpassword, PASSWORD_DEFAULT)));
+
+// check if the passwords match
+function password_validation($studentpassword, $expected_hashed_password) {
+	$cmp_result = strcmp("$expected_hashed_password","$studentpassword");
+	return $cmp_result;
+}
+
+echo json_encode(password_validation($studentpassword, $expected_hashed_password));
 mysqli_close($conn);
 
 ?>
