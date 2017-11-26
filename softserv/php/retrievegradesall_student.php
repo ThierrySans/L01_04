66a797c14ba88ec7d5792eaa6d4f43760cc20567
Mahesh209 <31778169+Mahesh209@users.noreply.github.com>
2017-11-26 15:19:15 -0500

    added comments

--- softserv/php/retrievegradesall_student.php
+++ softserv/php/retrievegradesall_student.php
@@ -1,14 +1,16 @@
 <?php
+/*
+This is a web service that retrieves all the grades of a single
+student from our database. It takes as input a username and returns
+all the grades for each problem set.
+*/
 header('Content-Type: application/json');
 include('./config.php');
 $student_id = $_GET["username"];
-//function getGrades($studentid) {
 $conn = mysqli_connect($servername, $username, $password, $db);
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
-
-//$sql_getgrades= "SELECT PROBLEMSETS.ID AS ID, UNITID, UNITS.NAME AS UNITNAME, PROBLEMSETS.NAME AS NAME, DATEDUE, HighestScore, RecentScore FROM (PROBLEMSETGRADES RIGHT JOIN PROBLEMSETS ON ID = ProblemsetID AND StudentID = '$studentid') JOIN UNITS ON UNITS.ID = PROBLEMSETS.UNITID;";
 
 $sql_getgrades = "SELECT o.UNITID AS UNITID, o.UNITNAME AS UNITNAME, o.ID AS ID, o.NAME AS NAME, o.DATEDUE AS DATEDUE, p.HighestScore AS HighestScore, o.HighestScore AS HighestScoreAvg, p.RecentScore AS RecentScore, o.RecentScore AS RecentScoreAvg FROM (SELECT UNITID, UNITNAME, t.ID AS ID, NAME, DATEDUE, HighestScore, RecentScore FROM (SELECT UNITID, UNITS.NAME AS UNITNAME, PROBLEMSETS.ID AS ID, PROBLEMSETS.NAME AS NAME, DATEDUE FROM PROBLEMSETS JOIN UNITS WHERE UNITS.ID = PROBLEMSETS.UNITID ORDER BY UNITID) AS t LEFT OUTER JOIN (SELECT AVG(HighestScore) AS HighestScore, AVG(RecentScore) AS RecentScore, ProblemSetID AS ID FROM PROBLEMSETGRADES GROUP BY ProblemSetID) AS s ON t.ID=s.ID) AS o JOIN (SELECT PROBLEMSETS.ID AS ID, UNITID, UNITS.NAME AS UNITNAME, PROBLEMSETS.NAME AS NAME, DATEDUE, HighestScore, RecentScore FROM (PROBLEMSETGRADES RIGHT JOIN PROBLEMSETS ON ID = ProblemsetID AND StudentID = '$student_id') JOIN UNITS ON UNITS.ID = PROBLEMSETS.UNITID) AS p ON o.ID=p.ID";
 
@@ -67,12 +69,6 @@ for ($i = 0; $i < count($return_getgrades); $i++) {
 		  "recentscoreavg" => $recentscoreavg);
 	$grades[$unitid]["unitname"] = $unitname;
 }
-	//mysqli_close($conn);
-	//return $grades;
-	
-//}
-//$student_id = $_GET["username"];
-//json_encode(getGrades($student_id));
 
 echo json_encode($grades);
 mysqli_close($conn);
