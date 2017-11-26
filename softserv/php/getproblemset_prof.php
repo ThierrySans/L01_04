c52e0210f9fc7b733f6b6250d10360ee838b5ce8
desslinga <fides.linga@mail.utoronto.ca>
2017-11-26 15:57:59 -0500

    creating first draft of v4 of product backlog

---
+++ softserv/php/getproblemset_prof.php
@@ -0,0 +1,40 @@
+<?php
+header('Content-Type: application/json');
+include('./config.php');
+$problemsetid = $_GET["problemsetid"];
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql_getquestions = "SELECT * FROM QUESTIONS WHERE PROBLEMSETID = '$problemsetid'";
+
+$result_getquestions = mysqli_query($conn, $sql_getquestions);
+
+$frommysql_getquestions = array(); //retrieve from assoc array
+
+// GET problemsets
+while ($row_getquestions = mysqli_fetch_assoc($result_getquestions)) {
+	$return_getquestions[] = $row_getquestions;
+}
+
+//function get_problems($return_getquestions){
+$questions = array();
+
+for ($i = 0; $i < count($return_getquestions); $i++) {
+	$questionid = $return_getquestions[$i]["ID"];
+	$questiontext = $return_getquestions[$i]["QUESTIONTEXT"];
+	$variabletext = $return_getquestions[$i]["VARIABLES"];
+	$answertext = $return_getquestions[$i]["ANSWER"];
+
+	$questions[$questionid] = array("text" => $questiontext,
+									"answer" => $answer,
+								    "variables" => $variabletext);
+	
+}
+
+echo json_encode($questions);
+mysqli_close($conn);
+
+?>
